3d12e7a1f37c9a5eb1616b41d24545b3529500b6
NikolasGrein <132360012+NikolasGrein@users.noreply.github.com>
2024-02-29 16:41:48 -0300

    Add files via upload

---
+++ Funcionário/src/main/java/com/func/entities/Departamento.java
@@ -0,0 +1,27 @@
+package com.func.entities;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "departamento")
+
+public class Departamento {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+}
